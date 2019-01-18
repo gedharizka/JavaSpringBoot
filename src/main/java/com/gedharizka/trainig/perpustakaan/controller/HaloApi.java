b8a2d476de89762ff1b9da8312cba4975c1fdd0c
gedharizka <gedharizka@gmail.com>
2019-01-18 09:21:14 +0700

    fitur save update delete list

--- src/main/java/com/gedharizka/trainig/perpustakaan/controller/HaloApi.java
+++ src/main/java/com/gedharizka/trainig/perpustakaan/controller/HaloApi.java
@@ -9,7 +9,7 @@ public class HaloApi {
     @GetMapping
     public String halo(){
         return "(\n" +
-                "\"message\": \"Halo ini dari Springboot2!\n\n" +
+                "\"message\": \"Halo ini dari Springboot2 setelah push ke Git!\n\n" +
                 ")";
     }
 }
--- src/main/java/com/gedharizka/trainig/perpustakaan/controller/PengarangApi.java
+++ src/main/java/com/gedharizka/trainig/perpustakaan/controller/PengarangApi.java
@@ -2,17 +2,53 @@ package com.gedharizka.trainig.perpustakaan.controller;
 
 import com.gedharizka.trainig.perpustakaan.entity.Pengarang;
 import com.gedharizka.trainig.perpustakaan.repository.PengarangRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/pengarang")
 public class PengarangApi {
 
+    @Autowired
     private PengarangRepository repository;
 
-
-    public Iterable<Pengarang>findAll(){
+    @GetMapping
+    public Iterable<Pengarang> findAll() {
         return repository.findAll();
     }
+
+    @PostMapping
+    public ResponseEntity<Pengarang>save(
+            @RequestBody Pengarang pengarang){
+        pengarang = repository.save(pengarang);
+        return ResponseEntity.ok(pengarang);
+    }
+
+    @GetMapping("/contoh")
+    public Pengarang init(
+            @RequestParam(name = "nama") String data1,
+            @RequestParam(name = "alamat", required = false) String data2) {
+        return new Pengarang(null, data1, data2);
+    }
+
+    @GetMapping("/(id)")
+    public ResponseEntity<Pengarang> findById(
+            @PathVariable("id") String pengarangId){
+        Optional<Pengarang> pengarangOptional = repository.findById(pengarangId);
+        if(pengarangOptional.isPresent())
+            return ResponseEntity.ok(pengarangOptional.get());
+        else
+            return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("{/id}")
+    public ResponseEntity<?> deleteById(String id){
+        repository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
 }
+
+
